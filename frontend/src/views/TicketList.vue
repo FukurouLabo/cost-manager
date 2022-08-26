ead58d81a45b1b1c4f3467462f737b06e793b2cf
daichi.iwamoto <tay1fai0y@gmail.com>
2022-08-26 17:00:51 +0900

    一覧スタイリング修正 リストのスタイリング修正 sassの導入

--- frontend/src/views/TicketList.vue
+++ frontend/src/views/TicketList.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="about">
     <h1>チケット一覧</h1>
-    <div v-if="display">
-      <p>{{message}}</p>
-    </div>
-
-    <div v-for="ticket in ticketList" :key="ticket.name">
-      <p>{{ticket.name}}</p>
-      <p>{{ticket.url}}</p>
-      <p>{{ticket.number}}</p>
+    <div class="ticket-list">
+      <div v-for="ticket in ticketList" :key="ticket.name" class="ticket" @click="selectTicket(ticket.name)">
+        <div class="header">
+          <p class="id">{{ticket.number}}</p>
+          <div class="title">
+            <p class="name">{{ticket.name}}</p>
+            <p class="url">{{ticket.url}}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,31 +19,84 @@
 export default {
   data() {
   return {
-      display: false,
-      message: '',
       ticketList: null,
     }
   },
   mounted() {
     window.backend
-      .basic()
-      .then((res) => {
-        this.display = true;
-        this.message = res
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    
-    window.backend
       .testTicketList()
       .then((res) => {
         this.ticketList = res
-        console.log(res)
       })
       .catch((err) => {
         console.log(err);
       });
   },
+  methods: {
+    selectTicket(name) {
+      alert(name);
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.ticket-list * {
+  color: #344563;
+}
+.ticket-list {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  .ticket {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 3px;
+    border: solid #344563 1px;
+    cursor: pointer;
+    transition: .3s;
+    &:hover {
+      opacity: .7;
+    }
+    .header {
+      display: flex;
+      justify-content: flex-start;
+      width: 100%;
+      .id {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #344563;
+        height: auto;
+        color: #fff;
+        font-weight: bold;
+        margin: 0;
+        padding: 20px;
+      }
+      .title {
+        display: flex;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+        .name {
+          width: 100%;
+          font-size: 20px;
+          font-weight: bold;
+          text-align: left;
+          margin: 0;
+          padding: 20px 20px 0;
+        }
+        .url {
+          width: 100%;
+          text-align: left;
+          margin: 0;
+          padding: 5px 20px 20px;
+        }
+      }
+    }
+  }
+}
+</style>
