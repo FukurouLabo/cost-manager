6f6b45a3835feb7fd3400038a9c8a20a75cbb5b0
daichi.iwamoto <tay1fai0y@gmail.com>
2022-08-27 11:33:32 +0900

    不要ファイルの削除・ルートの修正

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,12 @@
 import {createMemoryHistory, createRouter} from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Test from '../views/IssueList.vue'
+import IssueList from '../views/IssueList.vue'
 
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // You can only use pre-loading to add routes, not the on-demand loading method.
-    component: About
-  },
-  {
-    path: '/test',
-    name: 'Test',
-    component: Test
+    name: 'Top',
+    component: IssueList
   }
 ]
 
