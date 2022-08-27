12b7813386b36d2324ac3e13cb8f89090522860e
daichi.iwamoto <tay1fai0y@gmail.com>
2022-08-27 22:17:13 +0900

    デバック時のスタイルを、アプリの時に合わせる

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,25 +2,28 @@
   <router-view />
 </template>
 
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss">
+html {
+  background-color: #131313;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 }
 </style>
